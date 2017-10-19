f23a15789d1a3360b9e2671590d9e3cc3da1e79e
Zach DeCook <zachdecook@gmail.com>
2017-10-19 13:25:38 -0400

    * Starting block: Create "Equals" block

--- generators/sheets/sheetsblocks.js
+++ generators/sheets/sheetsblocks.js
@@ -2,9 +2,8 @@
  * @fileoverview Generating Sheets language for sheets blocks.
  * @author zachdecook@gmail.com (Zach DeCook)
  */
-Blockly.JavaScript['sheets_start'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+Blockly.Sheets['sheets_start'] = function(block) {
+  var value_name = Blockly.Sheets.valueToCode(block, 'NAME', Blockly.Sheets.ORDER_ATOMIC);
+  var code = '=' + value_name;
   return code;
 };
