dfa39c2f9c60c9f9dafab45afaae6883d0ee7097
Jesse Kuntz <jessekuntz96@gmail.com>
2017-11-09 12:56:38 -0500

    Changed nothing

--- generators/sheets/sheetsblocks.js
+++ generators/sheets/sheetsblocks.js
@@ -10,7 +10,13 @@ Blockly.Sheets['sheets_start'] = function(block) {
 
 Blockly.Sheets['sheets_cell'] = function(block) {
   var text_column = block.getFieldValue('column');
+  if (text_column.length > 1 || !(text_column.charCodeAt(0) >= 65 && text_column.charCodeAt(0) <= 90)) {
+    return "FAIL COLUMN";
+  }
   var text_row = block.getFieldValue('row');
+  if ((text_row.charCodeAt(0) === 48) || isNaN(text_row)) {
+    return "FAIL ROW";
+  }
   var value_inputcell = Blockly.Sheets.valueToCode(block, 'inputCell', Blockly.Sheets.ORDER_ATOMIC);
   var code = text_column + text_row;
   // TODO: Change ORDER_NONE to the correct strength.
