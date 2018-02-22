a0e9c1f16334a9f7b5d71989b6b1d128d06f106f
andrewthomas <andrewdt97@gmail.com>
2018-02-22 12:40:42 -0500

    Added sum block successfully.

--- blocks/sheetsblocks.js
+++ blocks/sheetsblocks.js
@@ -1,6 +1,7 @@
 /**
  * @fileoverview Blocks used for sheets.
  * @author zachdecook@gmail.com (Zach DeCook)
+ * @author andrewdt97@gmail.com (Andrew Thomas)
  */
 Blockly.Blocks['sheets_start'] = {
   init: function() {
@@ -10,6 +11,19 @@ Blockly.Blocks['sheets_start'] = {
     this.setInputsInline(false);
     this.setColour(255);
  this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sheets_sum'] = {
+  init: function() {
+    this.appendValueInput("range")
+        .setCheck(null)
+        .appendField("Sum");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(230);
+ this.setTooltip("Sum requires a range block");
  this.setHelpUrl("");
   }
 };
--- generators/sheets/sheetsblocks.js
+++ generators/sheets/sheetsblocks.js
@@ -1,9 +1,16 @@
 /**
  * @fileoverview Generating Sheets language for sheets blocks.
  * @author zachdecook@gmail.com (Zach DeCook)
+ * @author andrewdt97@gmail.com	(Andrew Thomas)
  */
 Blockly.Sheets['sheets_start'] = function(block) {
   var value_name = Blockly.Sheets.valueToCode(block, 'NAME', Blockly.Sheets.ORDER_ATOMIC);
   var code = '=' + value_name;
+  return code;
+};
+
+Blockly.Sheets['sheets_sum'] = function(block) {
+  var value_range = Blockly.Sheets.valueToCode(block, 'range', Blockly.Sheets.ORDER_ATOMIC);
+  var code = 'sum(' + value_range + ')';
   return code;
 };
